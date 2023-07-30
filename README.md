c1bcf26fecde88ee285101e1929170b6d374968d
DavidLim528 <jxl1238@gmail.com>
2023-07-29 21:41:10 -0700

    Partitioned into separate functions

--- README.md
+++
@@ -1,5 +0,0 @@
-# FRC-Signin
-
-To-do:\n
-  Change Days to full date: July 18, 2023\n
-  Hook it up to google form\n
--- generate.py
+++ generate.py
@@ -10,69 +10,83 @@ import datetime
 #second row is under first row
 
 workbook = openpyxl.Workbook()
-
-# Select the active sheet
 sheet = workbook.active
 sheet.title = "Week 1"
 
-year = 2023
-month = 7
-day = 18
-
-start_date = datetime.datetime(year, month, day)
 inc_delta = datetime.timedelta(days=1)
 week_delta = datetime.timedelta(days=7)
 
+def writeDayHeaders(start_date):
+    headers = []
+    # store dates from start time
+    for i in range(5):
+        headers.append(start_date.strftime("%A, %B %d, %Y"))
+        start_date += inc_delta
+    # Write headers
+    column = 1
+    header_num = 0
+    # Fill in color for each column
+    for i in range(5):
+        sheet.cell(row=1, column=column, value=headers[header_num]).fill = PatternFill(start_color='2273D1'
+                                                                                    , end_color='2273D1', fill_type='solid')
+        column += 4
+        header_num += 1
+    start_cell = "A1"
+    end_cell = "D1"
 
-headers = []    #"Tuesday", "","", "", 
-                #"Wednesday", "","", "",
-                #"Thursday", "","","",  
-                #"Friday", "","", "",   
-                #"Saturday", "","", "", 
 
-for i in range(5):
-    headers.append(start_date.strftime("%A, %B %d, %Y"))
-    start_date += inc_delta
+    # Merge Day columns
+    for i in range(5):
+            sheet.merge_cells(f"{start_cell}:{end_cell}")
+            start_cell = chr(ord(start_cell[0])+4) + "1"
+            end_cell = chr(ord(end_cell[0])+4) + "1"
+            
+    # Set width of name columns
+    sheet.column_dimensions['B'].width = 22
+    sheet.column_dimensions['F'].width = 22
+    sheet.column_dimensions['J'].width = 22
+    sheet.column_dimensions['N'].width = 22
+    sheet.column_dimensions['R'].width = 22
 
-# Data for the 3 columns beneath each cell of the main column
-sub_column_data = ["Arrival", "Name", "# in Party", "Reason",
+def writeSubheaders():
+    sub_column_data = ["Arrival", "Name", "# in Party", "Reason",
                    "Arrival", "Name", "# in Party", "Reason",
                    "Arrival", "Name", "# in Party", "Reason",
                    "Arrival", "Name", "# in Party", "Reason",
                    "Arrival", "Name", "# in Party", "Reason"]
 
-# Write headers
-column = 1
-header_num = 0
-# Fill in color for each column
-for i in range(5):
-    sheet.cell(row=1, column=column, value=headers[header_num]).fill = PatternFill(start_color='2273D1'
-                                                                                   , end_color='2273D1', fill_type='solid')
-    column += 4
-    header_num += 1
+    sheet.append(sub_column_data)
 
-start_cell = "A1"
-end_cell = "D1"
+def saveWorkbook(filename):
+    cd = os.getcwd()
+    filename = filename
+    file_path = os.path.join(cd, filename)
 
+    workbook.save(file_path)
+    return 1
 
-# Merge Day columns
-for i in range(5):
-        sheet.merge_cells(f"{start_cell}:{end_cell}")
-        start_cell = chr(ord(start_cell[0])+4) + "1"
-        end_cell = chr(ord(end_cell[0])+4) + "1"
+if __name__ == "__main__":
+    # Date inputs
+    # 7/25/2023
+    while True:
+        try:
+            year = int(input(">>Enter Year: "))
+            month = int(input(">>Enter Month: "))
+            day = int(input(">>Enter a Tuesday: "))
 
-sheet.append(sub_column_data)
-
-# Set width of name columns
-sheet.column_dimensions['B'].width = 22
-sheet.column_dimensions['F'].width = 22
-sheet.column_dimensions['J'].width = 22
-sheet.column_dimensions['N'].width = 22
-sheet.column_dimensions['R'].width = 22
-
-cd = os.getcwd()
-filename = "Fall2024.xlsx"
-file_path = os.path.join(cd, filename)
-
-workbook.save(file_path)
+            start_date = datetime.datetime(year, month, day)
+            break
+        except ValueError as v:
+            print("!! Invalid input")
+    
+    writeDayHeaders(start_date)
+    writeSubheaders()
+    
+    filename = input(">>Enter the name of your file: ")
+    filename += ".xlsx"
+    
+    if saveWorkbook(filename) != 1:
+        print(">>Failed to save file")
+        exit()
+    print(">>Success")
 
